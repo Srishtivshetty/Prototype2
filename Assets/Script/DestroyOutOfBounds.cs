b3ab55d53454c2901617abc9fb5815efa2d72de1
Srishtivshetty <srishti.shetty@indivillage.co>
2025-10-24 15:28:55 +0530

    Destroy projectiles offscreen

--- Assets/Script/DestroyOutOfBounds.cs
+++ Assets/Script/DestroyOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30;
+    private float topBound = 30.0f;
     //Start is called before the first frame update
     void Start()
     {
@@ -15,7 +15,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     void Update()
     {
         if (transform.position.z > topBound){
-            DestroyOutOfBounds(gameObject);   
+            Destroy(gameObject);   
         }
     }
 }
