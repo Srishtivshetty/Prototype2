134f1df8459fafb105491fc9376be717aff95125
Srishtivshetty <srishti.shetty@indivillage.co>
2025-10-24 14:53:13 +0530

    Spacebar press

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public float xRange = 10.0f;
 
     public GameObject projectilePrefab;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +29,10 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+        }
     }
 }
