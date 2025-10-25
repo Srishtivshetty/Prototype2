ebdceb7bb45959f39eedfc5dcfcd6f0cd1e399a6
Srishtivshetty <srishti.shetty@indivillage.co>
2025-10-25 09:21:56 +0530

    Spawn animals pressing S to make it work

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -19,10 +19,14 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            //Randomly generate animal ndex and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); 
+            SpawnRandomAnimal();
+                
         }
+    }
+     void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
